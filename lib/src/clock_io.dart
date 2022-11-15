52aeb711a2fea73ef01752f2f3b45139eea831c0
boyan01 <17426470+boyan01@users.noreply.github.com>
2022-11-15 21:38:56 +0800

    windows support

--- lib/src/clock_io.dart
+++ lib/src/clock_io.dart
@@ -4,52 +4,72 @@ import 'dart:io';
 import 'package:ffi/ffi.dart';
 
 import 'c_time_bindings_generated.dart';
+import 'win_system_clock_time_bindings_generated.dart';
 
-DynamicLibrary _openLibrary() {
-  if (Platform.isWindows) {
-    return DynamicLibrary.open("system_clock_plugin.dll");
-  }
-  return DynamicLibrary.process();
+abstract class SystemClock {
+  /// Duration since boot, not counting time spent in deep sleep.
+  Duration uptime();
+
+  /// Duration since boot, including time spent in sleep.
+  Duration elapsedRealtime();
 }
 
-final _library = _openLibrary();
+class UnixSystemClock implements SystemClock {
+  final _binding = TimeBindings(DynamicLibrary.process());
 
-final _binding = TimeBindings(_library);
+  final _isLinux = Platform.isLinux || Platform.isAndroid;
 
-final _isLinux = Platform.isLinux || Platform.isAndroid;
+  @override
+  Duration uptime() {
+    final ts = malloc<timespec>();
 
-///
-/// Duration since boot, not counting time spent in deep sleep.
-///
-Duration uptime() {
-  final ts = malloc<timespec>();
-
-  if (_isLinux) {
-    // CLOCK_MONOTONIC
-    _binding.clock_gettime(1, ts);
-  } else {
-    // CLOCK_UPTIME_RAW
-    _binding.clock_gettime(8, ts);
+    if (_isLinux) {
+      // CLOCK_MONOTONIC
+      _binding.clock_gettime(1, ts);
+    } else {
+      // CLOCK_UPTIME_RAW
+      _binding.clock_gettime(8, ts);
+    }
+    final d = ts.ref.tv_sec * 1000000000 + ts.ref.tv_nsec;
+    malloc.free(ts);
+    return Duration(microseconds: d ~/ 1000);
   }
-  final d = ts.ref.tv_sec * 1000000000 + ts.ref.tv_nsec;
-  malloc.free(ts);
-  return Duration(microseconds: d ~/ 1000);
+
+  @override
+  Duration elapsedRealtime() {
+    final ts = malloc<timespec>();
+
+    if (_isLinux) {
+      // CLOCK_BOOTTIME
+      _binding.clock_gettime(7, ts);
+    } else {
+      // CLOCK_MONOTONIC
+      _binding.clock_gettime(6, ts);
+    }
+    final d = ts.ref.tv_sec * 1000000000 + ts.ref.tv_nsec;
+    malloc.free(ts);
+    return Duration(microseconds: d ~/ 1000);
+  }
 }
 
-///
-/// Duration since boot, including time spent in sleep.
-///
-Duration elapsedRealtime() {
-  final ts = malloc<timespec>();
+class WinSystemClock implements SystemClock {
+  final _binding = WinClockBindings(DynamicLibrary.open('system_clock.dll'));
 
-  if (_isLinux) {
-    // CLOCK_BOOTTIME
-    _binding.clock_gettime(7, ts);
-  } else {
-    // CLOCK_MONOTONIC
-    _binding.clock_gettime(6, ts);
+  @override
+  Duration uptime() {
+    final d = _binding.system_clock_GetTickCount();
+    return Duration(microseconds: d * 1000);
   }
-  final d = ts.ref.tv_sec * 1000000000 + ts.ref.tv_nsec;
-  malloc.free(ts);
-  return Duration(microseconds: d ~/ 1000);
+
+  @override
+  Duration elapsedRealtime() {
+    return uptime();
+  }
 }
+
+final SystemClock systemClock =
+    Platform.isWindows ? WinSystemClock() : UnixSystemClock();
+
+Duration uptime() => systemClock.uptime();
+
+Duration elapsedRealtime() => systemClock.elapsedRealtime();
